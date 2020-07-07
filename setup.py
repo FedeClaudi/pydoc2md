acd2de4f5fbf376c03815baf0f3002919162b115
FedeClaudi <federicoclaudi@protonmail.com>
2020-07-07 11:56:52 +0100

    bump

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
-requirements = ["mdutils", "astunparse"]
+requirements = ["mdutils", "astunparse", "treelib"]
 
 setup(
     name="pydoc2md",
-    version="0.0.0.1",
+    version="0.0.0.2",
     description="Converts python code to markdown docs",
     long_description=long_description,
     long_description_content_type="text/markdown",
